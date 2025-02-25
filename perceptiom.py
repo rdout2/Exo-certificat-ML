d10350d3bcf5e5a91cd5aaeae2645632903112e5
unknown <rahmatmahamatdout@gmail.com>
2025-02-25 12:45:54 +0100

    peagos

--- perceptiom.py
+++ perceptiom.py
@@ -1,23 +1,3 @@
-##Perceptron Single Step Update
-def hinge_loss_single(feature_vector, label, theta, theta_0):
-    """
-    Computes the hinge loss on a single data point given the feature vector, label,
-    and parameters theta and theta_0.
-    """
-    loss = max(0, 1 - label * (np.dot(theta, feature_vector) + theta_0))
-    return loss
-
-
-def hinge_loss_full(feature_matrix, labels, theta, theta_0):
-    """
-    Computes the hinge loss on a dataset.
-    """
-    total_loss = 0
-    for i in range(len(labels)):
-        total_loss += hinge_loss_single(feature_matrix[i], labels[i], theta, theta_0)
-
-    average_loss = total_loss / len(labels)
-    return average_loss
 
 
 def perceptron_single_step_update(feature_vector, label, current_theta, current_theta_0):
